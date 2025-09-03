ab3b0916d3cce5ad77d73f7a5169180b04277612
Jesse Woo <woojesse@gmail.com>
2025-09-02 21:48:41 -0700

    migration files

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,103 @@
+from django import forms
 from django.db import models
-from wagtail.models import Page
-from wagtail.fields import RichTextField
 
-# keep the definition of BlogIndexPage model, and add the BlogPage model:
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
+
+from wagtail.models import Page, Orderable
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import MultiFieldPanel, FieldPanel
+from wagtail.snippets.models import register_snippet
+
+from slugify import slugify
+
+
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
+
+    content_panels = Page.content_panels + ["intro"]
+
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'BlogPage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE
+    )
+
 
 class BlogPage(Page):
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
+    authors = ParentalManyToManyField('blog.Author', blank=True)
 
-    content_panels = Page.content_panels + ["date", "intro", "body"]
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    # Add the main_image method:
+    def main_image(self):
+        gallery_item = self.gallery_images.first()
+        if gallery_item:
+            return gallery_item.image
+        else:
+            return None
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel([
+            "date", 
+            FieldPanel("authors", widget=forms.CheckboxSelectMultiple),
+            "tags"  
+        ], 
+        heading="Blog information"),
+        "intro", "body", "gallery_images"
+    ]
+
+
+class BlogPageGalleryImage(Orderable):
+    page = ParentalKey(BlogPage, on_delete=models.CASCADE, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = ["image", "caption"]    
+
+@register_snippet
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    author_image = models.ForeignKey(
+        'wagtailimages.Image', null=True, blank=True,
+        on_delete=models.SET_NULL, related_name='+'
+    )
+
+    panels = ["name", "author_image"]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Authors'
+
+class BlogTagIndexPage(Page):
+    def get_context(self, request):
+
+        # Filter by tag
+        tag = request.GET.get('tag')
+        blogpages = BlogPage.objects.filter(tags__name=tag)
+
+        # Update template context
+        context = super().get_context(request)
+        context['blogpages'] = blogpages
+        return context
